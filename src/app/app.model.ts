930af562a2838b73a7b59f08bda1425e523eaeb8
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-05-17 19:03:48 +0530

    Create header

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,10 +1,35 @@
 import { Bar, BarState } from "./components/bar-panel/bar.model";
 
-export class App{
-    numBars : number = 150;
-    bars : Bar[];
+export class App {
+    numBars: number = 150;
+    bars: Bar[];
+    isSorting : boolean = false;
 
-    constructor(){
-        this.bars = Array.from(Array(this.numBars).keys()).map(value => new Bar(value+1,BarState.unprocessed));
+    constructor() {
+        this.bars = Array.from(Array(this.numBars).keys()).map(value => new Bar(value + 1, BarState.unprocessed));
+    }
+
+    randomize() {
+        this.shuffle(this.bars);
+        this.isSorting = false;
+    }
+
+    toggleSorting(){
+        this.isSorting = !this.isSorting;
+    }
+
+    private shuffle(array: Bar[]) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  host:{class:'header'}
+  host: { class: 'header' }
 })
 export class HeaderComponent implements OnInit {
+
+  @Output() randomizeClicked : EventEmitter<void> = new EventEmitter();
+  @Output() toggleSortingClicked : EventEmitter<void> = new EventEmitter();
+  @Input() isSorting : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  randomize(){
+    this.randomizeClicked.emit();
+  }
+
+  toggleSorting(){
+    this.toggleSortingClicked.emit();
   }
 
 }
