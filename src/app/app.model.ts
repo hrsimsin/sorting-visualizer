af114c0a258caf26536bd6f99f6569a3713832d6
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-05-19 16:06:33 +0530

    Implement selection sort and insertion sort

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -9,7 +9,7 @@ export class App {
 
     barPanel: BarPanel = new BarPanel([], false);
     header: Header = new Header();
-    menu: Menu = new Menu(50, 150, 1, 70, 300, 1, ['Bubble Sort', 'Selection Sort', 'Insertion Sort'], 0);
+    menu: Menu = new Menu(25, 150, 1, 10, 1000, 1, ['Bubble Sort', 'Selection Sort', 'Insertion Sort'], 0);
     sortSteps: Array<Bar[]> = [];
     stepsHandler : any;
 
@@ -42,17 +42,64 @@ export class App {
     }
 
     private selectionSort() {
-
+        const arr = JSON.parse(JSON.stringify(this.barPanel.bars));
+        for(var i=0;i<arr.length;++i){
+            var pos=-1;
+            var min=-1;
+            for(var j=i;j<arr.length;++j){
+                arr[j].state = BarState.processing;
+                this.saveSortState(arr);
+                arr[j].state=BarState.unprocessed;
+                if(pos==-1){
+                    pos=j;
+                    min=arr[j].value;
+                }
+                else if(arr[j].value<min){
+                    min=arr[j].value;
+                    pos=j;
+                }
+            }
+            arr[pos].state = BarState.processing;
+            arr[i].state = BarState.processing;
+            this.saveSortState(arr);
+            var temp = arr[pos];
+            arr[pos]=arr[i];
+            arr[i]=temp;
+            this.saveSortState(arr);
+            arr[i].state = BarState.processed;
+            if(i!==pos)
+                arr[pos].state = BarState.unprocessed;
+        }
+        arr[arr.length-1].state = BarState.processed;
+        this.saveSortState(arr);
     }
 
     private insertionSort() {
-
+        var arr = JSON.parse(JSON.stringify(this.barPanel.bars));
+        arr[0].state=BarState.processed;
+        this.saveSortState(arr);
+        for(var i=1;i<arr.length;++i){
+            arr[i].state=BarState.processing;
+            this.saveSortState(arr);
+            var j=i;
+            while(j>0 && arr[j-1].value > arr[j].value){
+                var temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                this.saveSortState(arr);
+                --j;
+            }
+            arr[j].state=BarState.processed;
+        }
+        arr = arr.map((item:Bar) => {item.state=BarState.processed; return item});
+        this.saveSortState(arr);
     }
 
 
     private genSortSteps() {
         this.sortSteps = [];
         const sortFunctions = [this.bubbleSort.bind(this), this.selectionSort.bind(this), this.insertionSort.bind(this)];
+        this.barPanel.bars = this.barPanel.bars.map(item => {item.state=BarState.unprocessed; return item});
         sortFunctions[this.menu.selectedAlgorithmIndex]();
     }
 
@@ -88,6 +135,9 @@ export class App {
             this.barPanel.bars = (this.sortSteps.shift() as Bar[]);
             this.barPanel.isShuffled = false;
             this.barPanel = {...this.barPanel};
+        }
+        else{
+            this.header.isSorting = false;
         }
     }
 
